4281f726b6578ffc78338f936f834fe5838fc298
AdamBrownBB <adam.brown.betty@gmail.com>
2022-11-05 16:45:41 -0400

    add cheese frontend

---
+++ public/precache-manifest.53aebc8d7f0eeced7367fc5624e8687d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a1db74c841b914726ad96648ae621df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dec00b047d635098e7b0",
+    "url": "/static/css/main.b381a844.chunk.css"
+  },
+  {
+    "revision": "769aef9681d43e0236b6",
+    "url": "/static/js/2.9dd77702.chunk.js"
+  },
+  {
+    "revision": "3d9880ee0da2a100dc4a6223e0aed39a",
+    "url": "/static/js/2.9dd77702.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dec00b047d635098e7b0",
+    "url": "/static/js/main.0488e690.chunk.js"
+  },
+  {
+    "revision": "2104bbfd97023f182284",
+    "url": "/static/js/runtime-main.e8812808.js"
+  }
+]);
